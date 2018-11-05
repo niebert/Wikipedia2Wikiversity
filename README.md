972f8c2c060e6f2a840b0cf7a9d9f62484dca440
Engelbert Niehaus <niehaus@uni-landau.de>
2018-11-05 11:55:29 +0100

    wtf_fetch included in Wikipedia2Wikiversity

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Copy text fragments of Wikipedia into the textarea of the demo and press the 'Co
 * [Link Converter Demo](https://niebert.github.io/Wikipedia2Wikiversity)
 
 The web source of the Demo link mentioned above is stored in the subdirectory `/docs`. Please feel free to fork the repository and alter the source code according to your needs. To publish YOUR link converter on Github as well, change the link in this README.md to your repository.
+
+
+## See also
+* [Wikipedia2Wikiversity](https://niebert.github.io/Wikipedia2Wikiversity) uses  [`wtf_fetch`](https://niebert.github.io/Wikipedia2Wikiversity) to download the Wikipedia sources from the MediaWiki-API and convert the links for application in Wikiversity.
+* [`wtf_wikipedia`](https://github.com/spencermountain/wtf_wikipedia/) is the source repository developed by [Spencer Kelly](https://github.com/spencermountain), who created that great library for Wikipedia article processing which contain `wtf_fetch` as submodule.
+* [Wiki2Reveal](https://niebert.github.io/Wiki2Reveal) that uses `wtf_fetch` and `wtf_wikipedia` to download Wikipedia sources and convert the wiki sources "on-the-fly" into a RevealJS presentation (see [Wiki2Reveal Example](https://niebert.github.io/Wiki2Reveal/wiki2reveal.html?domain=wikiversity&title=Normen%2C+Metriken%2C+Topologie&author=Funktionalanalysis&language=de) ).
---
+++ docs/js/wiki_replace.js
@@ -0,0 +1,65 @@
+
+function replaceStringReverse(pString,pReplace,pSearch)
+//###### replaces in the string "pString" multiple substrings "pSearch" by "pReplace"
+{
+	return replaceString(pString,pSearch,pReplace);
+}
+
+function replaceString(pString,pSearch,pReplace)
+//###### replaces in the string "pString" multiple substrings "pSearch" by "pReplace"
+{
+	//alert("cstring.js - replaceString() "+pString);
+	if (pString != '') {
+		var vHelpString = '';
+        var vN = pString.indexOf(pSearch);
+		var vReturnString = '';
+		while (vN >= 0)
+		{
+			if (vN > 0)
+				vReturnString += pString.substring(0, vN);
+			vReturnString += pReplace;
+            if (vN + pSearch.length < pString.length) {
+				pString = pString.substring(vN+pSearch.length, pString.length);
+			} else {
+				pString = ''
+			}
+			vN = pString.indexOf(pSearch);
+		};
+	};
+	return vReturnString + pString;
+}
+
+
+function el(pID) {
+  return document.getElementById(pID)
+}
+
+function convertBracketLinks(pString)
+{
+  var vPrefix = el("prefix_wiki").value;
+  // {{Further|Wikipedia:Ebola virus epidemic in Sierra Leone}}
+  pString = pString.replace(/\{\{Further\|([^\|\}:]+)\}\}/g,"{{Further|"+el("prefix_wiki").value+":$1}}");
+  pString = pString.replace(/\{\{Further\|([^\|\}:]+)\|([^\|\]:]+)\}\}/g,"{{Further|"+vPrefix+":$1|"+vPrefix+":$2}}");
+ //{{Main|Wikipedia:West African Ebola virus epidemic timeline of reported cases and deaths}}
+  pString = pString.replace(/\{\{Main\|([^\|\}:]+)\}\}/g,"{{Main|"+vPrefix+":$1}}");
+ //{{Main article|
+ pString = pString.replace(/\{\{Main article\|([^\|\}:]+)\}\}/g,"{{Main article|"+vPrefix+":$1}}");
+ //  {{See also|Ebola virus epidemic in West Africa timeline}}
+ pString = pString.replace(/\{\{See also\|([^\|\}:]+)\}\}/g,"{{See also|"+vPrefix+":$1}}");
+ return pString
+}
+
+function convertLinks(pString)
+{
+  var vPrefix = el("prefix_wiki").value;
+  // {{Further|Wikipedia:Ebola virus epidemic in Sierra Leone}}
+  //pString = pString.replace(/\{\{Further\|([^\|\}:]+)\}\}/g,"{{Further|"+el("prefix_wiki").value+":$1}}");
+  //pString = pString.replace(/\{\{Further\|([^\|\}:]+)\|([^\|\]:]+)\}\}/g,"{{Further|"+el("prefix_wiki").value+":$1|"+el("prefix_wiki").value+":$2}}");
+  pString = convertBracketLinks(pString);
+  pString = pString.replace(/\[\[([^\|\]:]+)\|([^\|\]:]+)\]\]/g,"[["+vPrefix+":$1|$2]]");
+	pString = pString.replace(/\[\[([^\|\]:]+)\]\]/g,"[["+vPrefix+":$1|$1]]");
+	//pString = pString.replace(/\[\[([^\|]+)|([^\|\]]+)\]\]/g,"[[Wikipedia:$1|$2]]");
+	//pString = pString.replace(/\[\[([A-Za-z0-9 \-\/]+)|([A-Za-z0-9 \-\/]+)\]\]/g,"[[Wikipedia:$1|$2]]");
+
+	return pString
+}
